afd8bccd24776ea4eabe8403399958faa4b832f9
visualtuner <visualtuner@gmail.com>
2024-07-26 01:58:24 +0900

    페이지 스와이퍼 작업중

--- assets/js/main.js
+++ assets/js/main.js
@@ -83,7 +83,11 @@ $('#header nav button:nth-of-type(5)').on('click', function(){
 });
 
 
-
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+/* 2021 ------------------------------------------------------------------------------------------------ */
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
 var project2021Swiper = new Swiper(".project2021-swiper", {
     speed: 800,
     slidesPerView: 1,
@@ -103,14 +107,104 @@ var project2021Swiper = new Swiper(".project2021-swiper", {
         },
         slideChangeTransitionEnd: function(){
             let act = this.activeIndex;
-            if(act == 1){
+            if(act == 2){
                 $('.project2021-swiper .scroll-container .cont-wrap > .fade:first-of-type').addClass('in');
             }
         },
     }
 });
 
-var works2021Project1SceneSwiper = new Swiper(".works-2021 .scene-swiper", {
+$('.project2021-swiper .scroll-container').on('scroll', function(event){
+    let middlePoint = $(this).outerHeight() / 2;
+    let scrollT = $(this).scrollTop();
+    $(this).find('.cont-wrap > .fade').each(function(index){
+        let thisPosT = $(this).position().top;
+        
+        if(thisPosT < middlePoint + scrollT){
+            $(this).addClass('in');
+        }
+    });
+});
+
+
+
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+/* 2022 ------------------------------------------------------------------------------------------------ */
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+var project2022Swiper = new Swiper(".project2022-swiper", {
+    speed: 800,
+    slidesPerView: 1,
+    nested: true,
+    direction: "vertical",
+    allowTouchMove: false,
+    on: {
+        init: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2022 .nav-area .btn-item').removeClass('act');
+            $('.works-2022 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChange: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2022 .nav-area .btn-item').removeClass('act');
+            $('.works-2022 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChangeTransitionEnd: function(){
+            let act = this.activeIndex;
+            if(act == 2){
+                $('.project2022-swiper .scroll-container .cont-wrap > .fade:first-of-type').addClass('in');
+            }
+        },
+    }
+});
+
+$('.project2022-swiper .scroll-container').on('scroll', function(event){
+    let middlePoint = $(this).outerHeight() / 2;
+    let scrollT = $(this).scrollTop();
+    $(this).find('.cont-wrap > .fade').each(function(index){
+        let thisPosT = $(this).position().top;
+        
+        if(thisPosT < middlePoint + scrollT){
+            $(this).addClass('in');
+        }
+    });
+});
+
+
+
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+/* 2023 ------------------------------------------------------------------------------------------------ */
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+var project2023Swiper = new Swiper(".project2023-swiper", {
+    speed: 800,
+    slidesPerView: 1,
+    nested: true,
+    direction: "vertical",
+    allowTouchMove: false,
+    on: {
+        init: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2023 .nav-area .btn-item').removeClass('act');
+            $('.works-2023 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChange: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2023 .nav-area .btn-item').removeClass('act');
+            $('.works-2023 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChangeTransitionEnd: function(){
+            let act = this.activeIndex;
+            if(act == 2){
+                $('.project2023-swiper .scroll-container .cont-wrap > .fade:first-of-type').addClass('in');
+            }
+        },
+    }
+});
+
+var project2023P1MockupSwiper = new Swiper(".project2023-1 .mockup-swiper", {
     speed: 400,
     slidesPerView: "auto",
     spaceBetween: 0,
@@ -118,21 +212,182 @@ var works2021Project1SceneSwiper = new Swiper(".works-2021 .scene-swiper", {
     direction: "vertical",
     allowTouchMove: true,
     mousewheel: {
-        eventsTarget: '.project2021-1',
+        eventsTarget: '.project2023-1',
     },
     pagination: {
-        el: ".works-2021 .scene-swiper + .swiper-pagination",
+        el: ".project2023-1 .mockup-swiper + .swiper-pagination",
     },
     on: {
         init: function(){
             let act = this.activeIndex + 1;
-            $(".description-area .description-wrap").removeClass('act');
-            $(".description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+            $(".project2023-1 .description-area .description-wrap").removeClass('act');
+            $(".project2023-1 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
         },
         slideChange: function(){
             let act = this.activeIndex + 1;
-            $(".description-area .description-wrap").removeClass('act');
-            $(".description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+            $(".project2023-1 .description-area .description-wrap").removeClass('act');
+            $(".project2023-1 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+        },
+        slideChangeTransitionEnd: function(){
+            let act = this.activeIndex;
+            if(act == 2){
+                this.mousewheel.disable();
+            }else{
+                this.mousewheel.enable();
+            }
+        },
+    }
+});
+
+var project2023P2MockupSwiper = new Swiper(".project2023-2 .mockup-swiper", {
+    speed: 400,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    resistanceRatio: 0,
+    direction: "vertical",
+    allowTouchMove: true,
+    mousewheel: {
+        eventsTarget: '.project2023-2',
+    },
+    pagination: {
+        el: ".project2023-2 .mockup-swiper + .swiper-pagination",
+    },
+    on: {
+        init: function(){
+            let act = this.activeIndex + 1;
+            $(".project2023-2 .description-area .description-wrap").removeClass('act');
+            $(".project2023-2 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+        },
+        slideChange: function(){
+            let act = this.activeIndex + 1;
+            $(".project2023-2 .description-area .description-wrap").removeClass('act');
+            $(".project2023-2 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+        },
+        slideChangeTransitionEnd: function(){
+            this.mousewheel.disable();
+        },
+    }
+});
+
+
+$('.project2023-swiper .scroll-container').on('scroll', function(event){
+    let middlePoint = $(this).outerHeight() / 2;
+    let scrollT = $(this).scrollTop();
+    $(this).find('.cont-wrap > .fade').each(function(index){
+        let thisPosT = $(this).position().top;
+        
+        if(thisPosT < middlePoint + scrollT){
+            $(this).addClass('in');
+        }
+    });
+});
+
+
+$('.project2023-swiper .scroll-container').on("mousewheel DOMMouseScroll", function (event){
+    if( $(this).scrollTop() == 0 ){
+        if (event.originalEvent.wheelDelta >= 0) {
+            project2023Swiper.slideTo(1);
+        }
+    }
+});
+
+$('.project2023-1').on("mousewheel DOMMouseScroll", function (event){
+    if (event.originalEvent.wheelDelta >= 0) {
+        project2023P1MockupSwiper.mousewheel.enable();
+    } else {
+        project2023Swiper.slideTo(1);
+    }
+});
+
+$('.project2023-2').on("mousewheel DOMMouseScroll", function (event){
+    if(project2023P2MockupSwiper.activeIndex == 0){
+        if (event.originalEvent.wheelDelta <= 0) {
+            project2023P2MockupSwiper.mousewheel.enable();
+        } else {
+            project2023P2MockupSwiper.mousewheel.disable();
+            project2023Swiper.slideTo(0);
+        }
+    }else if(project2023P2MockupSwiper.activeIndex == 1){
+        if (event.originalEvent.wheelDelta >= 0) {
+            project2023P2MockupSwiper.mousewheel.enable();
+        } else {
+            project2023P2MockupSwiper.mousewheel.disable();
+            project2023Swiper.slideTo(2);
+        }
+    }
+});
+
+
+$('.works-2023 .nav-area .btn-item:nth-of-type(1)').on('click', function(){
+    project2023Swiper.slideTo(0);
+});
+
+$('.works-2023 .nav-area .btn-item:nth-of-type(2)').on('click', function(){
+    project2023Swiper.slideTo(1);
+});
+
+$('.works-2023 .nav-area .btn-item:nth-of-type(3)').on('click', function(){
+    project2023Swiper.slideTo(2);
+});
+
+
+
+
+
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+/* 2024 ------------------------------------------------------------------------------------------------ */
+/* ----------------------------------------------------------------------------------------------------- */
+/* ----------------------------------------------------------------------------------------------------- */
+var project2024Swiper = new Swiper(".project2024-swiper", {
+    speed: 800,
+    slidesPerView: 1,
+    nested: true,
+    direction: "vertical",
+    allowTouchMove: false,
+    on: {
+        init: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2024 .nav-area .btn-item').removeClass('act');
+            $('.works-2024 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChange: function(){
+            let act = this.activeIndex + 1;
+            $('.works-2024 .nav-area .btn-item').removeClass('act');
+            $('.works-2024 .nav-area .btn-item:nth-of-type('+ act +')').addClass('act');
+        },
+        slideChangeTransitionEnd: function(){
+            let act = this.activeIndex;
+            if(act == 1){
+                $('.project2024-swiper .scroll-container .cont-wrap > .fade:first-of-type').addClass('in');
+            }
+        },
+    }
+});
+
+var project2024P1MockupSwiper = new Swiper(".project2024-1 .mockup-swiper", {
+    speed: 400,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    resistanceRatio: 0,
+    direction: "vertical",
+    allowTouchMove: true,
+    mousewheel: {
+        eventsTarget: '.project2024-1',
+    },
+    pagination: {
+        el: ".project2024-1 .mockup-swiper + .swiper-pagination",
+    },
+    on: {
+        init: function(){
+            let act = this.activeIndex + 1;
+            $(".project2024-1 .description-area .description-wrap").removeClass('act');
+            $(".project2024-1 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
+        },
+        slideChange: function(){
+            let act = this.activeIndex + 1;
+            $(".project2024-1 .description-area .description-wrap").removeClass('act');
+            $(".project2024-1 .description-area .description-wrap:nth-of-type("+ act +")").addClass('act');
         },
         slideChangeTransitionEnd: function(){
             let act = this.activeIndex;
@@ -146,42 +401,40 @@ var works2021Project1SceneSwiper = new Swiper(".works-2021 .scene-swiper", {
 });
 
 
-$('.project2021-swiper .scroll-container').on('scroll', function(event){
+$('.project2024-swiper .scroll-container').on('scroll', function(event){
     let middlePoint = $(this).outerHeight() / 2;
     let scrollT = $(this).scrollTop();
     $(this).find('.cont-wrap > .fade').each(function(index){
         let thisPosT = $(this).position().top;
         
         if(thisPosT < middlePoint + scrollT){
-            console.log( index );
             $(this).addClass('in');
         }
     });
-
 });
 
 
-$('.project2021-swiper .scroll-container').on("mousewheel DOMMouseScroll", function (event){
+$('.project2024-swiper .scroll-container').on("mousewheel DOMMouseScroll", function (event){
     if( $(this).scrollTop() == 0 ){
         if (event.originalEvent.wheelDelta >= 0) {
-            project2021Swiper.slideTo(0);
+            project2024Swiper.slideTo(0);
         }
     }
 });
 
-$('.project2021-1').on("mousewheel DOMMouseScroll", function (event){
+$('.project2024-1').on("mousewheel DOMMouseScroll", function (event){
     if (event.originalEvent.wheelDelta >= 0) {
-        works2021Project1SceneSwiper.mousewheel.enable();
+        project2024P1MockupSwiper.mousewheel.enable();
     } else {
-        project2021Swiper.slideTo(1);
+        project2024Swiper.slideTo(1);
     }
 });
 
 
-$('.works-2021 .nav-area .btn-item:nth-of-type(1)').on('click', function(){
-    project2021Swiper.slideTo(0);
+$('.works-2024 .nav-area .btn-item:nth-of-type(1)').on('click', function(){
+    project2024Swiper.slideTo(0);
 });
 
-$('.works-2021 .nav-area .btn-item:nth-of-type(2)').on('click', function(){
-    project2021Swiper.slideTo(1);
+$('.works-2024 .nav-area .btn-item:nth-of-type(2)').on('click', function(){
+    project2024Swiper.slideTo(1);
 });
