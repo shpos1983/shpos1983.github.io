5becfb1b57b07184bf44595513115884815f3cde
이석구 <visualtuner@gmail.com>
2026-07-31 10:34:32 +0900

    타이틀

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,8 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SangSquare</title> 
+</svelte:head>
 {@render children()}
