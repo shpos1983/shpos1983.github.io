d9ad3d6553780727a618ed96824568fbc60a67b2
이석구 <visualtuner@gmail.com>
2026-07-31 10:25:15 +0900

    파비콘

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+</script>
+
+<h1>Who I am</h1>
+<p>I bring teams, user needs, and business goals together<br/>to build products people value.</p>
+<Button>Let's Talk</Button>
