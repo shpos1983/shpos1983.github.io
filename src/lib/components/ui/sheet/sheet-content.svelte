6139d1a96131ea0a8983396b0fe1f0dc655077fc
이석구 <visualtuner@gmail.com>
2026-08-10 09:32:50 +0900

    사이드 바 시트 클로즈 버튼 사이즈

--- src/lib/components/ui/sheet/sheet-content.svelte
+++ src/lib/components/ui/sheet/sheet-content.svelte
@@ -44,7 +44,7 @@
 		{#if showCloseButton}
 			<SheetPrimitive.Close data-slot="sheet-close">
 				{#snippet child({ props })}
-					<Button variant="ghost" class="absolute top-4 right-4 bg-secondary" size="icon-sm" {...props}>
+					<Button variant="ghost" class="absolute top-4 right-4 bg-secondary" size="icon-lg" {...props}>
 						<XIcon  />
 						<span class="sr-only">Close</span>
 					</Button>
